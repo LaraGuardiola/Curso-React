5807fac6aeab32e0ba623ab09309a25e9285b1a5
LaraGuardiola <webdev.slara@gmail.com>
2022-04-18 18:45:43 +0200

    fething correctly from elden ring api, added some styling

---
+++ apilist/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './components/App'
+
+//create a class based component that fetches pictures of dogs
+
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(<App />);
